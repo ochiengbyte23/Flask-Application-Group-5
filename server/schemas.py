6924c9d452b1c0af703988d8a75f3672faf08cbc
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-04-22 18:10:38 +0300

    Added marshmallow schemas for input validation and serialisation of role id

--- server/schemas.py
+++ server/schemas.py
@@ -6,3 +6,7 @@ class UserSchema(Schema):
     name = fields.String(required=True, validate=validate.Range(min=4)) 
     email = fields.String(required=True, error_messages={"required": "Email is required."}) 
     password = fields.String(validate=validate.Range(min=6, max=15))
+    
+class RoleSchema(Schema): 
+    role_id = fields.Integer(dump_only=True) 
+    role = fields.String(required=True)
