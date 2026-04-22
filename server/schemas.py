57c1ce9f9918d429679251d4f76fbbbc7b309e57
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2026-04-22 18:09:29 +0300

    Create UserSchema and RoleSchema with field validation

--- server/schemas.py
+++ server/schemas.py
@@ -1,2 +1,8 @@
 from marshmallow import Schema, fields, validate
 from models import User, Role
+
+class UserSchema(Schema): 
+    id = fields.Integer(dump_only=True) 
+    name = fields.String(required=True, validate=validate.Range(min=4)) 
+    email = fields.String(required=True, error_messages={"required": "Email is required."}) 
+    password = fields.String(validate=validate.Range(min=6, max=15))
